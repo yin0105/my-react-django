3b7aea41634f02641b0ef029d778e31c82f34dd5
solutionLi <momo821123@outlook.com>
2021-03-19 21:46:51 +0300

    Add style.css, Burger.js, RightNav.jsx

---
+++ frontend/src/containers/Layout/Navbars/Burger.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import RightNav from './RightNav';
+
+const StyledBurger = styled.div`
+  width: 30px;
+  height: 25px;
+  position: fixed;
+  top: 75px;
+  right: 20px;
+  z-index: 20;
+  display: none;
+  @media (max-width: 1200px) {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
+  }
+  div {
+    width: 25px;
+    height: 3px;
+    background-color: ${({ open }) => open ? '#ccc' : '#333'};
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+    &:nth-child(1) {
+      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+    }
+    &:nth-child(2) {
+      transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
+      opacity: ${({ open }) => open ? 0 : 1};
+    }
+    &:nth-child(3) {
+      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+    }
+  }
+`;
+
+const Burger = () => {
+  const [open, setOpen] = useState(false)
+  
+  return (
+    <>
+      <StyledBurger open={open} onClick={() => setOpen(!open)}>
+        <div />
+        <div />
+        <div />
+      </StyledBurger>
+      <RightNav open={open}/>
+    </>
+  )
+}
+
+export default Burger
---
+++ frontend/src/containers/Layout/Navbars/RightNav.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from "../../../components/CustomButton/CustomButton.jsx";
+import { NavLink } from "react-router-dom";
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  padding: 0px 30px;
+  position: relative;
+  top: -60px;
+  right: 15px;
+
+  li {
+    padding: 10px;
+  }
+
+  
+  @media (max-width: 1200px) {
+    flex-flow: column nowrap;
+    background-color: #0D2538;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    padding-top: 10px;
+    z-index: 100;
+    width: 256px;
+    height: 355px;
+    right: 0px;
+    top: 130px;
+    background: linear-gradient(180deg, #9ECD51 0%, #638F1B 100%);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    transition: transform 0.3s ease-in-out;
+    li {
+      color: #fff;
+    }
+
+    li {
+      padding: 12px 0px;
+      color: white;
+      border-bottom: #9CD146 solid 1px;
+    }
+
+    li:last-of-type {
+      border-bottom: none;
+    }
+
+    li a span {
+      color: white !important;
+    }
+
+    a.link_as_btn {
+        border: none !important;
+        background: none !important;
+        padding-left: 0px !important;
+        min-width: 0px !important;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+  return (
+    <Ul open={open} >
+      <li>
+        <a href={"/login"}>
+          <span className="navbar-name">News</span>
+        </a>
+      </li>
+      <li>
+        <NavLink to={"/login"}>
+          <span className="navbar-name">Categories</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/login"}>
+          <span className="navbar-name">Companies</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/login"}>
+          <span className="navbar-name">Jobs</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/login"}>
+          <span className="navbar-name">Search</span>
+        </NavLink>
+      </li>
+      <li>
+        <a href={"/login"}>
+          <span className="navbar-name">Contact us</span>
+        </a>
+      </li>
+      <li className="btn-padding">
+          {/* <Button bsStyle="success" fill >Join now</Button> */}
+        <a href={"/login"} className="link_as_btn btn apply-button">
+          <span className="navbar-name">Join Now</span>
+        </a>
+      </li>
+
+    </Ul>
+  )
+}
+
+export default RightNav
